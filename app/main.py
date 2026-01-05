da7578292fb966f0ca4f97980a61209f13735116
KauaVictor077 <kaua38460@gmail.com>
2026-01-05 18:57:53 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Optional, Any
 
 
 class Person:
-    
+
     people: Dict[str, "Person"] = {}
 
     def __init__(self, name: str, age: int) -> None:
@@ -18,20 +18,25 @@ class Person:
 def create_person_list(people_list: List[Dict[str, Any]]) -> List[Person]:
     Person.people.clear()  # Clear any previous data
     result: List[Person] = []
+
     for person_dict in people_list:
         name = person_dict["name"]
         age = person_dict["age"]
         result.append(Person(name, age))
+
     # Link wives and husbands
     for person_dict in people_list:
         name = person_dict["name"]
         person_obj = Person.people[name]
+
         if person_dict.get("wife"):
             wife_name = person_dict["wife"]
             person_obj.wife = Person.people[wife_name]
             Person.people[wife_name].husband = person_obj
+
         if person_dict.get("husband"):
             husband_name = person_dict["husband"]
             person_obj.husband = Person.people[husband_name]
             Person.people[husband_name].wife = person_obj
+
     return result
